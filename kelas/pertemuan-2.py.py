a6fd0e5d08113d3a2a7d7af5804749e450a2cb92
AnahWiyana <Trubusiman2025.example.com>
2025-09-22 21:06:58 -0700

    Initial commit

---
+++ kelas/pertemuan-2.py.py
@@ -0,0 +1,51 @@
+
+#prodi = ['informatika', True, 1, 'tekniksipil']
+
+# angka = "123"
+# angkat = int(angka)
+
+# print(type(angka))
+# print(type(angkat))
+
+# nama = input("Masukan nama anda : ")
+
+# print(nama)
+
+# operator1 = not (1 > 5)
+# print (operator1)
+
+
+# studi kasus
+
+# nama_siswa = 'Daffa'
+# nilai_1 = 85.5
+# nilai_2 = 91.0
+# nilai_3 = 77.5
+# nilai_4 = 88.0
+
+# nilai_siswa = [nilai_1,nilai_2,nilai_3,nilai_4]
+
+# print(nilai_siswa)
+# total=sum(nilai_siswa)
+# print(total)
+
+# harga_barang=int(input('masukan harga barang: '))
+# pajak=(harga_barang*8.11)
+
+# harga_akhir=(harga_barang*pajak)
+
+# print('pajak yang harus di bayar: ')
+# print(harga_akhir)
+
+# print ("""Halo,
+# Saya sedang belajar ngoding
+# Bahasa Pemrograman python""")
+
+# warna = {'warna1' : 'merah',
+# 'warna2' : 'kuning',
+# 'warna3' : 'hijau'}
+
+# print(warna['warna1'])
+
+# nama = input('daffa')
+# print('daffa')
